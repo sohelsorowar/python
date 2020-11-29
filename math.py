ad604fa4f397dda22eee16dfd129a899a2140ea4
sohelsorowar <sohelsorowar@gmail.com>
2020-11-29 12:54:38 +0600

    smallest multiple

--- math.py
+++ math.py
@@ -16,12 +16,32 @@
 
 #5 cylender
 
-pi=22/7
-h=float(input("height: "))
-r=float(input("Radius"))
-v=pi*(r*r)*h
-print(v)
+# pi=22/7
+# h=float(input("height: "))
+# r=float(input("Radius"))
+# v=pi*(r*r)*h
+# print(v)
 
-A= (2*pi*r*h )+ (2*pi*(r*r))
+# A= (2*pi*r*h )+ (2*pi*(r*r))
 
-print(A)
+# print(A)
+
+#10 smallest multiple
+
+def smallest_multiple(n):
+    if (n<=2):
+        return n
+    i = n*2
+    factors= [number for number in range(n,1,-1) if number *2>n]
+    print(factors)
+
+    while True:
+        for a in factors:
+            if i % a !=0:
+                i+=n
+                break
+            if (a == factors[-1] and i % a == 0):
+                return i
+
+print(smallest_multiple(13))
+print(smallest_multiple(11))
