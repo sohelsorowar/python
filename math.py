ebeaac84b35692d08095a450e9cbbfe292217edf
sohelsorowar <sohelsorowar@gmail.com>
2020-12-03 12:27:01 +0600

    convert camel case string to snake case

--- math.py
+++ math.py
@@ -188,3 +188,20 @@ print("Print equilateral triangle Pyramid with characters ")
 #         print(character, end=' ')
 #         asciiNumber += 1
 #     print(" ")
+
+
+def change_case(str): 
+    res = [str[0].lower()] 
+ 
+    for c in str[1:]: 
+        if c in ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'): 
+            res.append('_') 
+            res.append(c.lower()) 
+        else: 
+            res.append(c) 
+      
+    return ''.join(res) 
+      
+# Driver code 
+str = "GeeksForGeeks"
+print(change_case(str)) 
