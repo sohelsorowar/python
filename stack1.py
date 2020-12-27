75f0ac0fe9dbb7df9cadcc28d139d04752af628d
sohelsorowar <sohelsorowar@gmail.com>
2020-12-27 23:55:37 +0600

    stack1

---
+++ stack1.py
@@ -0,0 +1,34 @@
+
+class Stack():
+    def __init__(self):
+        self.items=[]
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def get_stack(self):
+        return self.items
+    
+    def is_empty(self):
+        return self.items ==[]
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    
+s =Stack()
+s.push("A")
+s.push("B")
+s.push("C")
+s.push("D")
+
+print(s.get_stack())
+
+s.pop()
+print(s.get_stack())
+
+print(s.peek())
